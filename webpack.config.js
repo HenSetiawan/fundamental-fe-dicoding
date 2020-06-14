ba161e9a63a0900b0a3da8e0243947138bf1a998
HenSetiawan <56755687+HenSetiawan@users.noreply.github.com>
2020-06-14 17:28:55 +0700

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+ 
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "app.js"
+    },mode: "production",
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: "style-loader"
+                    },
+                    {
+                        loader: "css-loader"
+                    }
+                ]
+            }
+        ]
+    },
+       /* plugin */
+       plugins: [
+        /* HTML Webpack Plugin */
+        new HtmlWebpackPlugin({
+            template: "index.html",
+            filename: "index.html"
+        })
+    ],
+    //  devtool: 'source-map',
+    // devServer:{
+    //     contentBase: 'public'
+    //      }
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9000
+      }
+        }
+
+
